d4f36e7062a8bf7b2beb0a7119ce94625706b3e1
abdulkadir.demirel <kadirdemirel_17@hotmail.com>
2022-11-24 14:05:35 +0300

    e-commerce.sql edited.

--- workshops/e-commerce-database-design/e-commerce.sql
+++ workshops/e-commerce-database-design/e-commerce.sql
@@ -190,10 +190,10 @@ CREATE TABLE IF NOT EXISTS public.products
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     name character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    unit_price money NOT NULL,
     stock integer NOT NULL,
     description character varying(255) COLLATE pg_catalog."default" NOT NULL,
     image_url character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    unit_price integer NOT NULL,
     CONSTRAINT products_pkey PRIMARY KEY (id)
 );
 
