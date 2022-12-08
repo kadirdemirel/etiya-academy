577e4e7894eb3a811b0d399657da4db66d32ec44
abdulkadir.demirel <kadirdemirel_17@hotmail.com>
2022-12-08 10:06:34 +0300

    Mehods with magic string was edited.

--- e-commerce-demo-pair-7/src/main/java/com/etiya/ecommercedemopair7/business/constants/Messages.java
+++ e-commerce-demo-pair-7/src/main/java/com/etiya/ecommercedemopair7/business/constants/Messages.java
@@ -2,7 +2,7 @@ package com.etiya.ecommercedemopair7.business.constants;
 
 public class Messages {
     public static final String categoryCanNotExistWithSameName = "Bu isimle bir kategori zaten mevcut!";
-    public static final String existsByCategoryId="İlgili kategori bulunamadı.";
-  public static final String  existsByProductId="İlgili ürün bulunamadı.";
-  public static final String existsBySellerId="İlgili satıcı bulunamadı.";
+    public static final String categoryNotFound="İlgili kategori bulunamadı.";
+  public static final String  productNotFound="İlgili ürün bulunamadı.";
+  public static final String sellerNotFound="İlgili satıcı bulunamadı.";
 }
